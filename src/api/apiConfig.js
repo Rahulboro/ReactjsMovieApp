be21e9e0c6fde7bcb6adc75d4837302865ca1789
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-06-07 23:04:48 +0530

    configure api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,7 @@
 const apiConfig = {
   baseeUrl: "https://www.themoviedb.org/",
   apiKey: "089ebbf783aa111ad5ceaa2abae369b1",
-  originalImage: (imfPath) => ``,
+  // originalImage: (imfPath) => ``,
 };
+
+export default apiConfig;
