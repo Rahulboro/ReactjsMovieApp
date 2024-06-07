a73dbb133eb52f0456cc2e621f5d1f3cb374b44a
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-06-07 23:57:22 +0530

    'created tmdb api  '

---
+++ src/api/tmdbApi.js
@@ -0,0 +1,24 @@
+import axiosClient from "./axiosClient";
+
+export const category = {
+  movie: "movie",
+  tv: "tv",
+};
+
+export const movieType = {
+  upcoming: "upcoming",
+  popular: "popular",
+  top_rated: "top_rated",
+};
+
+export const TvType = {
+  upcoming: "upcoming",
+  popular: "popular",
+  on_the_air: "on_the_air",
+};
+
+const tmbApi = {
+  getMoviesList: (type, params) => {
+    const url= 'movie' +
+  }
+}
